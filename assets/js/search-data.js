9e6852081b5635c627ce2ffa930f16dff23b004a
parsamajidi21 <117867910+parsamajidi21@users.noreply.github.com>
2025-12-02 11:25:28 +0000

    Deploying to gh-pages from @ parsamajidi21/parsamajidi21.github.io@a94caca401676af767cef9d8d578ae1896574125 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-neural-network-implementaiton-in-fpga-using-hls",
+        
+          title: "Neural Network implementaiton in FPGA using HLS",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/dnn-fpga/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
