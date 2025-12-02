bc6091086bf5778f81b40d239cd5ec1e079dc110
parsamajidi21 <117867910+parsamajidi21@users.noreply.github.com>
2025-12-02 13:15:04 +0000

    Deploying to gh-pages from @ parsamajidi21/parsamajidi21.github.io@953b1150bf9f06533fbf67ced0d23cded61e9adc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,29 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-neural-network-implementaiton-in-fpga-using-hls",
+        
+          title: "Neural Network implementaiton in FPGA using HLS",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/dnn-fpga/";
+          
+        },
+      },{id: "post-neural-network-implementaiton-in-fpga-using-hls",
+        
+          title: "Neural Network implementaiton in FPGA using HLS",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/images/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
@@ -411,17 +433,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2015/code/";
-          
-        },
-      },{id: "post-a-post-with-images",
-        
-          title: "a post with images",
-        
-        description: "this is what included images could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/images/";
           
         },
       },{id: "post-a-post-with-formatting-and-links",
