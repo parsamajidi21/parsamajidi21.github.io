eafddfb5a74fd12191208bb7fe7fcbc6ce7e9f35
parsamajidi21 <117867910+parsamajidi21@users.noreply.github.com>
2025-12-02 14:37:54 +0000

    Deploying to gh-pages from @ parsamajidi21/parsamajidi21.github.io@74e53cc5aa75a8c563a8c22160ba26e45ef1f930 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -162,28 +162,42 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/cv.pdf", "_blank");
+          window.open("/assets/pdf/example_pdf.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%6D%61%6A%69%64%69%69.%70%61%72%73%61@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-github',
-        title: 'GitHub',
+        id: 'social-inspire',
+        title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://github.com/https://github.com/parsamajidi21", "_blank");
+          window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
+        id: 'social-rss',
+        title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.linkedin.com/in/https://www.linkedin.com/in/parsamajidi/", "_blank");
+          window.open("/feed.xml", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
